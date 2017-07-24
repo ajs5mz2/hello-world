f2ba556f80ebd7661c1659897cdbecb27f495ed4
Arthur SMith <30415010+ajs5mz2@users.noreply.github.com>
2017-07-24 13:33:11 -0400

    Updated files to remove syntax error

---
+++ hello-world.java
@@ -0,0 +1,8 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+        for (int i=1; i<=10; i++) {
+			System.out.println(i);
+	    }
+    }
+}
